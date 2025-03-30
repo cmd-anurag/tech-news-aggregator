a981f042870c092619b00fdc32a459ed2dad60d2
cmd-anurag <anuragkumar.7157@gmail.com>
2025-03-30 17:53:59 +0530

    i forgor

--- app/article/[id]/page.tsx
+++ app/article/[id]/page.tsx
@@ -1,3 +1,8 @@
 export default function ArticlePage({params} : { params: {id: string}}) {
-    
+    return (
+        <div>
+          <h1>Article ID: {params.id}</h1>
+        {/* 🥱 */}
+        </div>
+      );
 }
